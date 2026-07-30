21722030b8a5576fd20c86774cdf1e27ff681483
RedCyberDragon <51099384+smitht06@users.noreply.github.com>
2026-07-29 20:12:06 -0400

    Revert "revert"
    This reverts commit 2aa23f5d8a38160001cf3fe3b4425925a65ea112.
    
    Revert "shadow run char app"
    
    This reverts commit 62047eb873067c2957d62a448a4d0adc8ffd1902.
    
    Revert "added url"
    
    This reverts commit aec8869411b03d1fd53f53069d4301a0e8596bb4.
    
    Revert "added url"
    
    This reverts commit 67378ee32b7bf4765496d23e5506a5014ae64ffe.
    
    Revert "added url"
    
    This reverts commit c6c0b4b1d413b3f69b064291ab691085b08459ec.

--- shadowrun_char/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShadowrunCharConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "shadowrun_char"
-    verbose_name = "Shadowrun Character Generator"
